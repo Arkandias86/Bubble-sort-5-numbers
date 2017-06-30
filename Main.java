ce782dbed28e074526cc3500852e3fbcd93459bb
Arkandias86 <nicolashuet1@gmail.com>
2017-06-30 20:09:37 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char tableauCaractere[] = {'a','b','c','d','e','f','g'};
+	for (int i = 0; i < tableauCaractere.length; i++)
+	    System.out.println("A l'emplacement "+i+" se trouve la lettre "+tableauCaractere[i]);
+    }
+}
